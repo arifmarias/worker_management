ff6c027a28be387075b959d0e8eda116cb0e2a17
Mohammed Arif <arif.sg.my@gmail.com>
2024-02-25 23:02:09 +0800

    lable Change

--- pages/1_🙆🏻‍♂️_WORKER PROFILE.py
+++ pages/1_🙆🏻‍♂️_WORKER PROFILE.py
@@ -118,7 +118,7 @@ if selected == "Search":
     worker_name = df["worker_name"].drop_duplicates().sort_values(ascending=False)
     lst_worker_name = list(worker_name)
     lst_worker_name.insert(0,"Select")
-    w_name = st.selectbox(options = lst_worker_name, label="")
+    w_name = st.selectbox(options = lst_worker_name, label="Select Worker Name")
     if w_name!='Select':
         selected_worker = df[df["worker_name"] == w_name]
         cols = st.columns(3)
