fb3310bc552ef71301dc1535b3d11c793b6f1731
Mohammed Arif <arif.sg.my@gmail.com>
2024-02-26 21:05:26 +0800

    Check DF

--- 0_🏠_HOME.py
+++ 0_🏠_HOME.py
@@ -61,23 +61,24 @@ current_day = current_date.day
 current_year_month = str(current_year) + "-" + str(current_month)
 total_salary_remain = 0
 total_pay_out = 0
-total_number_of_workers = df_info['worker_name'].drop_duplicates().count()
-total_base_salary = df_info['worker_base_salary'].sum()
-for items in df_info.worker_name.unique():
-    view_df = df_salary[(df_salary['worker_name'] == items) & (df_salary['year_month']==current_year_month)]
-    if not view_df.empty:
-        match_df = pd.DataFrame(view_df.sort_values("salary_remain",ascending=True).head(1))
-        salary_remain = match_df["salary_remain"].sum()
-        pay_out = view_df["today_pay"].sum()
-        total_salary_remain +=salary_remain
-        total_pay_out +=pay_out
+if not df_info.empty:
+    total_number_of_workers = df_info['worker_name'].drop_duplicates().count()
+    total_base_salary = df_info['worker_base_salary'].sum()
+    for items in df_info.worker_name.unique():
+        view_df = df_salary[(df_salary['worker_name'] == items) & (df_salary['year_month']==current_year_month)]
+        if not view_df.empty:
+            match_df = pd.DataFrame(view_df.sort_values("salary_remain",ascending=True).head(1))
+            salary_remain = match_df["salary_remain"].sum()
+            pay_out = view_df["today_pay"].sum()
+            total_salary_remain +=salary_remain
+            total_pay_out +=pay_out
     
     
 
-cols = st.columns(3)
-with cols[0]:
-    ui.metric_card(title="Number of Workers", content=str(total_number_of_workers), description="Working in AD HCARE ", key="card1")
-with cols[1]:
-    ui.metric_card(title="Total Salary", content=f"RM {total_base_salary:,}", description="For All Workers", key="card2")
-with cols[2]:
-    ui.metric_card(title="Paid Out", content=f"RM {total_pay_out:,}", description="This Month", key="card3")
+    cols = st.columns(3)
+    with cols[0]:
+        ui.metric_card(title="Number of Workers", content=str(total_number_of_workers), description="Working in AD HCARE ", key="card1")
+    with cols[1]:
+        ui.metric_card(title="Total Salary", content=f"RM {total_base_salary:,}", description="For All Workers", key="card2")
+    with cols[2]:
+        ui.metric_card(title="Paid Out", content=f"RM {total_pay_out:,}", description="This Month", key="card3")
