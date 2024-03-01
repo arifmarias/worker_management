2b54b9af59c55a3d92686500f5d676ba75646d10
Mohammed Arif <arif.sg.my@gmail.com>
2024-03-01 22:03:49 +0800

    edit

--- pages/1_🙆🏻‍♂️_WORKER PROFILE.py
+++ pages/1_🙆🏻‍♂️_WORKER PROFILE.py
@@ -205,8 +205,7 @@ if selected == "Edit Worker Information":
                     #worker_state = selectbox("Select State",my_states,no_selection_label=selected_worker['worker_state'].values[0])
                     result = selectbox(
                                 "Select an option with different label",
-                                ["Kuala Lumpur", "Putrajaya", "Labuan","Selangor","Malacca","Negeri Sembilan","Penang","Johor","Kedah",
-                                 "Kelantan"],
+                                ["Kuala Lumpur", "Putrajaya", "Labuan","Selangor","Malacca","Negeri Sembilan","Penang","Johor","Kedah"],
                                 no_selection_label=str(selected_worker['worker_state'].values[0]),
                             )
                     st.write(result)
