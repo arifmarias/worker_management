80e4293cfed1c82fa7d4c311d159b7537aa235a4
Mohammed Arif <arif.sg.my@gmail.com>
2024-03-01 19:19:24 +0800

    edit

--- pages/1_🙆🏻‍♂️_WORKER PROFILE.py
+++ pages/1_🙆🏻‍♂️_WORKER PROFILE.py
@@ -177,6 +177,7 @@ if selected == "Edit Worker Information":
                                             'Terengganu','Perlis','Sabah','Sarawak']
             
         selected_worker = df[df["worker_name"] == w_name]
+        edited_df = st.experimental_data_editor(selected_worker)
 #         st.text_input("Worker Name",selected_worker['worker_name'].values[0])
 #         "---"
 #         #st.dataframe(selected_worker)
