10cc0243bf8c7b094f48f3ef4d571e1732ca98d2
Mohammed Arif <arif.sg.my@gmail.com>
2024-03-01 22:42:09 +0800

    search

--- pages/1_🙆🏻‍♂️_WORKER PROFILE.py
+++ pages/1_🙆🏻‍♂️_WORKER PROFILE.py
@@ -147,10 +147,10 @@ if selected == "Search":
             ui.metric_card(title="Name", content=selected_worker["worker_name"].values[0], description="Join in "+selected_worker['worker_joining_date'].values[0]+","+selected_worker['worker_gender'].values[0]+","+"\n Mobile: "+selected_worker['worker_phone_number'].values[0], key="card1")
         cols = st.columns(1)
         with cols[0]:
-            ui.metric_card(title="Passport Details", content=selected_worker["worker_passport"].values[0], description="Expiry Date: "+selected_worker['worker_pass_expiry'].values[0]+"\n Visa Expiry: "+selected_worker['worker_visa_expiry'].values[0], key="card2")
+            ui.metric_card(title="Passport Details", content=selected_worker["worker_passport"].values[0], description="Expiry Date: "+selected_worker['worker_pass_expiry'].values[0]+"\n\nVisa Expiry: "+selected_worker['worker_visa_expiry'].values[0], key="card2")
         cols = st.columns(1)
         with cols[0]:
-            ui.metric_card(title="Workplace", content=selected_worker["worker_current_company"].values[0], description="Joined "+selected_worker['worker_currentcompany_joindate'].values[0]+"\n Address: "+selected_worker['worker_current_workplace'].values[0], key="card3")
+            ui.metric_card(title="Workplace", content=selected_worker["worker_current_company"].values[0], description="Joined "+selected_worker['worker_currentcompany_joindate'].values[0]+"\n\n Address: "+selected_worker['worker_current_workplace'].values[0], key="card3")
         cols2 = st.columns(1)
         with cols2[0]:
             ui.metric_card(title="Total Salary given Till Today", content=salary_worker.item(), key="card4")
